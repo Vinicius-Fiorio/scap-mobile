de6c321784ba079a417d2a73674225b4fd708e98
Lucas Oliveira <laobevilaqua@gmail.com>
2019-11-08 21:07:44 -0300

    retirando debugger

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -67,8 +67,6 @@ export class HomePage implements OnInit{
   }
   
   compartilharProcesso(processo: Processos) {
-
-    debugger
     this.socialSharing.share(`Veja detalhes desse processo:
     *Assunto*: ${ processo.assunto }
     *Tribunal*: ${ processo.tribunal }
